0f062cc6f02875adbf0c0698ef65f7393e2476e0
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-12-02 09:58:42 +0800

    Initial commit

---
+++ GoodFoodie_IPTPROJ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoodFoodie_IPTPROJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
