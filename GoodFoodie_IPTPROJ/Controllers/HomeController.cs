449c6c7fc167acb68e0fc4c2060ae93ec4bd6444
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-12-06 18:52:10 -0500

    landing page UI improvement

--- GoodFoodie_IPTPROJ/Controllers/HomeController.cs
+++ GoodFoodie_IPTPROJ/Controllers/HomeController.cs
@@ -16,9 +16,24 @@ namespace GoodFoodie_IPTPROJ.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+            var products = new List<ProductModel>
+        {
+                new ProductModel { ProductId = 1, ImageUrl = "pictures/L1.png", ProductName = "Product 1", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 2, ImageUrl = "pictures/L1.png", ProductName = "Product 2", Price = 19.99, StarRatings = 3 },
+                new ProductModel { ProductId = 3, ImageUrl = "pictures/L1.png", ProductName = "Product 3", Price = 19.99, StarRatings = 3 },
+                new ProductModel { ProductId = 4, ImageUrl = "pictures/L1.png", ProductName = "Product 4", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 5, ImageUrl = "pictures/L1.png", ProductName = "Product 5", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 6, ImageUrl = "pictures/L1.png", ProductName = "Product 6", Price = 19.99, StarRatings = 3 },
+                new ProductModel { ProductId = 7, ImageUrl = "pictures/L1.png", ProductName = "Product 7", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 8, ImageUrl = "pictures/L1.png", ProductName = "Product 8", Price = 19.99, StarRatings = 3 },
+                new ProductModel { ProductId = 9, ImageUrl = "pictures/L1.png", ProductName = "Product 9", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 10, ImageUrl = "pictures/L1.png", ProductName = "Product 10", Price = 19.99, StarRatings = 4 },
+                new ProductModel { ProductId = 11, ImageUrl = "pictures/L1.png", ProductName = "Product 11", Price = 19.99, StarRatings = 3 },
+                new ProductModel { ProductId = 12, ImageUrl = "pictures/L1.png", ProductName = "Product 12", Price = 19.99, StarRatings = 4 },
+        };
 
+            return View(products);
+        }
         [Authorize]
         public IActionResult Privacy()
         {
@@ -30,5 +45,8 @@ namespace GoodFoodie_IPTPROJ.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
+
+
 }
---
+++ GoodFoodie_IPTPROJ/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GoodFoodie_IPTPROJ.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int StarRatings { get; set; }
+    }
+}
