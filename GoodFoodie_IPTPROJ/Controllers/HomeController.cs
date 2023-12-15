e64274745cea37aaa4dcd23539b8d420a868a0c5
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-12-15 17:53:47 +0800

    Add more pages

--- GoodFoodie_IPTPROJ/Controllers/HomeController.cs
+++ GoodFoodie_IPTPROJ/Controllers/HomeController.cs
@@ -40,6 +40,19 @@ namespace GoodFoodie_IPTPROJ.Controllers
             return View();
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
